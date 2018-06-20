c170f7012028e0c86664c972bcec81a1dbcbb420
Vadim Sieviertsev <vadikpointvadik@gmail.com>
2018-06-20 18:37:41 +0300

    Angular HW

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/article-details/article-details.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-art-detail',
+  templateUrl: './article-details.component.html',
+  styleUrls: ['./article-details.component.css']
+})
+export class ArticleDetailsComponent {
+    article={
+      id : 1,
+      title:'Як знімали рекламу Apple в Україні',
+      shortDescription:'Режисер кліпу Rolling in the Deep, скейтер з Іспанії та оператор на роликах з Південної Африки.'
+    };
+     heading: string;
+
+     isClicked:boolean = true;
+
+    changeColor(){
+      this.isClicked=!this.isClicked;
+    }
+}
